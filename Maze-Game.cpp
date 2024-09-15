74d8073d180021f240373566ed5ada4fd8fd4789
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-09-16 00:13:20 +0300

    added comments and made the flags 1 bit

--- Maze-Game.cpp
+++ Maze-Game.cpp
@@ -4,35 +4,37 @@
 
 class Cell {
 private:
-	uint8_t xCoord;
-
-	uint8_t yCoord;
-
-	bool isVisited;
-
-	bool isWalkable;
+	uint8_t xCoord; //cordinate x of the cell using 1 byte
+	uint8_t yCoord; //coordinate y of the cell using 1 byte
+	bool isVisited : 1; //1 bit for visited flag
+	bool isWalkable : 1; //1 bit for walkable flag
 
 public:
+	//constructor to initialize the cell
 	Cell(uint8_t x, uint8_t y) : xCoord(x), yCoord(y), isVisited(false), isWalkable(false) {}
-
+	
+	//getters for x and y coordinates
 	uint8_t getX() const { return xCoord; }
-
 	uint8_t getY() const { return yCoord; }
 
+	//function to check if the cell has been visited
 	bool isVisited() const { return isVisited; }
 
+	//function to check if the cell is walkable(a.k.a not a wall)
 	bool isWalkable() const { return isWalkable; }
 };
 
 class Maze {
 private:
-	uint8_t size;
+	uint8_t size; //size of the maze using 1 bit
 
-	std::vector<std::vector<Cell>> maze;
+	std::vector<std::vector<Cell>> maze; //2D grid of cell objects
 
 public:
+	//constructor to initialize the maze
 	Maze(uint8_t s) : size(s) {}
 
+	//getter for the size of the maze
 	uint8_t getSize() const { return size; }
 };
 
